d4f5cc7a83439dc5195288d5d99fd03b73619cad
Rommel Rico <me@rommelrico.com>
2018-11-05 08:31:48 -0800

    Initializing the SeekBar and TimesTables ListView.

--- app/src/main/java/com/rommelrico/androidtimestables/MainActivity.kt
+++ app/src/main/java/com/rommelrico/androidtimestables/MainActivity.kt
@@ -3,6 +3,7 @@ package com.rommelrico.androidtimestables
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
+import android.widget.SeekBar
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initializing the SeekBar and TimesTables ListView.
+        val timesTablesSeekBar = findViewById<SeekBar>(R.id.timesTablesSeekBar)
+        timesTablesListView = findViewById(R.id.timesTablesListView)
+
     }
 }
